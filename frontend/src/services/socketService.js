e04b0e20c94b047ad3528da9a8dd832b9247aa0c
aryamanbaadkar1-sudo <aryamanbaadkar1@gmail.com>
2025-11-28 22:16:45 +0530

    Update socketService.js

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -11,7 +11,7 @@ class SocketService {
             return;
         }
 
-        const SOCKET_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+        const SOCKET_URL ='https://nebullink.onrender.com'; 
         this.socket = io(SOCKET_URL, {
             auth: { token }
         });
